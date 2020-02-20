90a301596d53933f22f7154bf90bd506e61a2f15
Nicholas Piperis <npiperis2017@fau.edu>
2020-02-19 22:40:17 -0500

    Modified home_index.spec.js cyress test.

--- cypress/integration/Home/home_index.spec.js
+++ cypress/integration/Home/home_index.spec.js
@@ -5,7 +5,14 @@ describe('Checks the content of the trivia app home page', () => {
     });
 
     it('Verifies text, buttons and elements in the page', () => {
+        cy.get('img.splash').should('exist');
+        
+        cy.get('input.landing-textfield').eq(0).should('have.attr', 'placeholder', 'Room Code');
+        cy.get('input.landing-textfield').eq(1).should('have.attr', 'placeholder', 'Name');
+        cy.get('input.landing-button').should('have.attr', 'type', 'button').should('have.value', 'Play');
+
         cy.get('h1.info-envelope-title').should('contain', 'Knowledge that sticks.');
-    });
+        cy.get('h2').should('contain', 'How does Witti work?');
+        });
 
 });
