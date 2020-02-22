59fa44637a6915baa58a336c0def82940c907ba4
CJ101 <goodalluigi@gmail.com>
2020-02-22 14:47:27 -0500

    Node server now executable from command line

---
+++ server.js
@@ -0,0 +1,48 @@
+
+    //  Clyde Goodall 2020
+
+    // ->init server 
+    //     ->pool user connections
+    //         ->fetch IDs
+    //         ->handle entering/leaving (using entry tokens represented as button ids)
+    //             ->if joining room, ask for room
+    //                 ->redirect user if code is correct
+    //             ->if creating room, skip 
+    //     ->user rooms (represent with objects. should be easier to handle properties)
+    //         ->generate room id 
+    //         ->handle entering/leaving
+    //             ->update array of current users in room.IDs
+    //                 ->pass client updated list
+    //         ->lobby
+    //             ->allow lobby owner to select topic
+    //         ->handle game status
+    //             ->when each user has accepted, start game
+    //                 ->set/reset scores
+    //                 ->wait for every user to answer, or a length of time (eg 30s)  }- (looped until end of question)
+    //                 ->pop question from stack                                      }
+    //                 ->winner declared
+    //                     ->wait for users to elect to start new game
+    //                     ->allow winner to pick new topic
+    //                 ->exit game, redirect to homepage (will be changed if we opt to have a gameover landing screenf or users to collect/save question answer sources)
+    //  * - completed
+    
+//includes xpress and initializes
+var express = require('express');
+var app = express();
+
+//Creates server, binds socket to server
+server = require('http').createServer(app)
+var io = require('socket.io')(server);
+console.log('Server initialized');
+
+
+//include resources
+app.use(express.static(__dirname + '/www/res'));
+
+//serves /www/, all files visible to client
+app.get('/', function(req, res) {
+    res.sendFile(__dirname + "/www/index.html");
+});
+console.log('Live, from /www/, its SATURDAY NIGHT LIVE');
+
+server.listen(4200);
