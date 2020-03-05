00b7cefb979a1a6d1dd3240be7fdf4bb0347cc16
CJ101 <goodalluigi@gmail.com>
2020-03-05 10:24:52 -0500

    mobile version support

--- server.js
+++ server.js
@@ -42,14 +42,14 @@ app.use(express.static(__dirname + '/www/res'));
 
 //serves /www/, all files visible to client
 app.get('/', function(req, res) {
-    res.sendFile(__dirname + "/www/index.html");
+    res.sendFile(__dirname + "/www/");
 });
 
 console.log('Live, from /www/, its SATURDAY NIGHT LIVE');
 
 
 //socket code
-var rooms = [
+var _rooms = [
 
   ];
 
@@ -57,36 +57,43 @@ function switchRoom(code, name, socket) {
   console.log(name + ' sent to room #' + code);
 
   //joins user to room, sends succes message, updates all users in the room
-  socket.join('code');
-  io.emit('join success', {
+  //socket.join(code);
+  io.to('{socket.id}').emit('join success', {
     code: code,
     name: name
   });
-  socket.to(code).emit('user joined', name);
+  //socket.to(code).emit('user joined', name);
   
 }
 
-function createRoom(code, name, socket) {
+function createRoom(code, name, socket, b) {
   console.log("Attempting room creation");
   //generates code for room since none was entered, causing a room to be created
-  var code = Math.floor(Math.random()*90000) + 10000;
+  if(b) { var code = Math.floor(Math.random()*90000) + 10000; }
 
   //adds room and info to registry array
-  rooms.append = [
-    [code, name + "'s Room"]
-  ];
+  _rooms.push(
+    [ code, name + "'s Room'", 
+      [
+        socket.id
+      ]
+]
+  );
 
   console.log(name + "'s room (" + code + ") successfully created. Added to array");
+  for(var i = 0; i < _rooms.length; i++) {
+    console.log("Room #" + i + ": " + _rooms[i][0]);
+  }
   switchRoom(code, name, socket);
 }
 
 function joinRoom(code, name, socket) {
   console.log('user attempting to join room ' + name);
-  if(rooms.length > 0) {
+  if(_rooms.length > 0) {
     //tests for match in room array
-    for(var i = 0; i < rooms.length; i++) {
+    for(var i = 0; i < _rooms.length; i++) {
 
-      if(code == rooms[i][0]) {
+      if(code == _rooms[i][0]) {
 
         //emits success event
         
@@ -94,33 +101,46 @@ function joinRoom(code, name, socket) {
         break;
         
       }
+      //no match yet
     }
+    //no match log
+  
   } else {
-
+    
     //creates room if user attempts to join room where none exist
     console.log('No available rooms, creating one now');
-    createRoom(code, name);
+    createRoom(code, name, false);
   }
 }
 
 io.on('connection', function(socket){
-    console.log('a user connected');
+    console.log( socket.id + ' connected');
 
     socket.on("join", function(info) {
-      console.log('attempting room join');
+      if(socket.rooms.indexOf(_rooms) >= 0) {
+        console.log('user already in room');
+      }
+       console.log('attempting to join room ' + info.code);
       joinRoom(info.code, info.name, socket);
     });
-  
+    
     socket.on("create", function(info) {
-
+      if(socket.rooms.indexOf(_rooms) >= 0) {
+        console.log('user already in room');
+      }
       console.log('attempting room creation: ' + info.name);
-      createRoom(info.code, info.name, socket);
+      createRoom(info.code, info.name, socket, true);
     });
 
     socket.on('disconnect', function(){
-    console.log('user disconnected');
+      
+      console.log(socket.id  + ' disconnected');
     });
   });
+
+  function isInRoom(socket) {
+
+  }
 
  
   
