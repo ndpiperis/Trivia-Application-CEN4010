1adca212f7f4386769ea7deba0add37797e00bf4
Nicholas Piperis <npiperis2017@fau.edu>
2020-04-15 17:40:39 -0400

    Modified quizcreator.js and added to data.json with more quizzes and content.

--- www/res/js/quizcreator.js
+++ www/res/js/quizcreator.js
@@ -1,5 +1,15 @@
+var socket = io();
+
 const fs = require('fs');
 
 let obj = {
     creatorQuiz: []
 };
+
+obj.creatorQuiz.push({id: 1, question: 1});
+
+var json = JSON.stringify(obj);
+
+var fs = require('fs');
+fs.writeFile('myjsonfile.json', json, 'utf8', callback);
+
