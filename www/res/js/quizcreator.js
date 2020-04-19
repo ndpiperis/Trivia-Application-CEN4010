12a9d76424cb268ec4c0c983c4c42cdef5c1a5dc
Nicholas Piperis <npiperis2017@fau.edu>
2020-04-18 21:15:50 -0400

    fixed answer for a question in data.json and trying to do something with quizcreator.js :/

--- www/res/js/quizcreator.js
+++ www/res/js/quizcreator.js
@@ -1,25 +1,24 @@
-var socket = io();
+//var socket = io();
 
-const fs = require('fs');
-
-let obj = {
+var obj = {
     creatorQuiz: []
 };
 
-obj.creatorQuiz.push({id: "", title: "", questions: [{q: "", img: "", source: "", opt1: "", opt2: "", opt3: "", opt4: "", answer: ""}]});
+obj.creatorQuiz.push({title: "", questions: [{q: "", img: "", source: "", opt1: "", opt2: "", opt3: "", opt4: "", answer: ""}], review:{correct:"false", explanation:"", source:"", makeup:{q:"", image:"", opt1:"", opt2:"", answer:""}, main:{q:"", image:"", source:"", opt1:"", opt2:"", answer:""}}});
 
 var json = JSON.stringify(obj);
 
 var fs = require('fs');
-fs.writeFile('myjsonfile.json', json, 'utf8', callback);
+//fs.writeFile('myjsonfile.json', json, 'utf8', callback);
 
-$(document).ready(function() {
-    $('.entry-form').submit(function(e) {
-        ntitle = $('#title').val()
-        nquestion = $('#question').val()
-        nimg = $('#img').val()
+fs.readFile('myjsonfile.json', 'utf8', function readFileCallback(err, data){
+    if (err){
+        console.log(err);
+    } else {
+    obj = JSON.parse(data); //now an object
+    obj.creatorQuiz.push({id: "", title: "", questions: [{q: "", img: "", source: "", opt1: "", opt2: "", opt3: "", opt4: "", answer: ""}]}); //add some data
+    json = JSON.stringify(obj); //converts it back to json
+    fs.writeFile('myjsonfile.json', json, 'utf8', callback); // write its back 
+}});
 
-        e.preventDefault();
-
-    });
-});
+console.log(obj);
