b486af28dde19e9f9468efe13a62343df018ec7b
Nicholas Piperis <npiperis2017@fau.edu>
2020-04-19 00:19:18 -0400

    Progress on appending existing json file for quiz creator :)

--- www/res/js/quizcreator.js
+++ www/res/js/quizcreator.js
@@ -1,22 +1,25 @@
-//var socket = io();
+const jsonfile = require('jsonfile')
 
-var obj = [];
+const file = 'datatest.json'
+const obj = {
+    id: "",
+    title: "",
+        questions: [
+        {
+            q: "",
+            img: "",
+            source: "",
+            opt1: "",
+            opt2: "",
+            opt3: "",
+            opt4: "",
+            answer: ""
+        }
+    ]
+}
 
-obj.creatorQuiz.push({title: "", questions: [{q: "", img: "", source: "", opt1: "", opt2: "", opt3: "", opt4: "", answer: ""}], review:{correct:"false", explanation:"", source:"", makeup:{q:"", image:"", opt1:"", opt2:"", answer:""}, main:{q:"", image:"", source:"", opt1:"", opt2:"", answer:""}}});
+JSON.stringify(obj, null, '\t')
 
-var json = JSON.stringify(obj);
-
-var fs = require('fs');
-//fs.writeFile('myjsonfile.json', json, 'utf8', callback);
-
-fs.readFile('datatest.json', 'utf8', function readFileCallback(err, data){
-    if (err){
-        console.log(err);
-    } else {
-    obj = JSON.parse(data); //now an object
-    obj.creatorQuiz.push({id: "", title: "", questions: [{q: "", img: "", source: "", opt1: "", opt2: "", opt3: "", opt4: "", answer: ""}]}); //add some data
-    json = JSON.stringify(obj); //converts it back to json
-    fs.writeFile('datatest.json', json, 'utf8', callback); // write its back 
-}});
-
-console.log(obj);
+jsonfile.writeFile(file, obj, { flag: 'a' }, function (err) {
+    if (err) console.error(err)
+  })
